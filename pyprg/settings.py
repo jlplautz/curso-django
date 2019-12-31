9b599bbc5e571e2dc1f5806f472a4216853f10ea
61246880 <jorge.plautz@gmail.com>
2019-12-31 08:01:38 -0300

    Instalado lib python decouple
    close #22

--- pyprg/settings.py
+++ pyprg/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -23,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w+!t3syk2g_pv(xq^_9!u9pu-l@^3t4wp6(0^^ig%j#8z1pqi!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
