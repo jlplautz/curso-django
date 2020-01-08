774340c7cff7e0e78a763d557d727187eb46f7ad
61246880 <jorge.plautz@gmail.com>
2020-01-08 08:16:41 -0300

    Alterado o html da home base
    close #67

--- pyprg/base/tests/test_home.py
+++ pyprg/base/tests/test_home.py
@@ -1,9 +1,16 @@
-from django.test import Client
+import pytest
+from django.urls import reverse
 
 from pyprg.django_assertions import assert_contains
 
 
-def test_status_code(client: Client):
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('home'))
+    return resp
+
+
+def test_status_code(resp):
     """
     quando accessamos nosso site fazemos uma chamada do tipo get
     logo na raiz da aplicação. Justamente a view que construimos
@@ -11,10 +18,12 @@ def test_status_code(client: Client):
     Emulando um request o protocolo http vai retornar um response.
     Aqui queremos nos certificar que esta resposta retornou com sucesso
     """
-    resp = client.get('/')
     assert resp.status_code == 200
 
 
-def test_title(client: Client):
-    resp = client.get('/')
+def test_title(resp):
     assert_contains(resp, '<title>Python Pro</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("home")}">Python Pro</a>')
