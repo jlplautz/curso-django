2b80ebde885a18ef12ff318ec3c746a370af1201
61246880 <jorge.plautz@gmail.com>
2020-01-08 12:50:55 -0300

    Encapsulado URLs na app
    close #69

--- pyprg/urls.py
+++ pyprg/urls.py
@@ -17,11 +17,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from pyprg.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include('pyprg.base.urls')),
 ]
 
 if settings.DEBUG:
