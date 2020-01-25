7c324ed02c3f3ee23cc926dcf0ff31124fa47719
61246880 <jorge.plautz@gmail.com>
2020-01-25 20:27:11 -0300

    Inserido a utilização da slug para mostrar os videos e tambem o conceito  de filtros
    close #126

--- pyprg/aperitivos/views.py
+++ pyprg/aperitivos/views.py
@@ -7,5 +7,9 @@ from django.shortcuts import render
 # o request que vai rendenrizar  a pagina do meu video.
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 386792293}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 386792293},
+        'instalacao-windows': {'titulo': 'Instalação Wndows', 'vimeo_id': 387145176},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
