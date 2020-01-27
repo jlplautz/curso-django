de3955c148854e1602bb762cf656864ba2587a64
61246880 <jorge.plautz@gmail.com>
2020-01-27 20:05:45 -0300

    Verificado acessoa metodo sem parametro no template
    close #143

--- pyprg/aperitivos/tests/tests_indice.py
+++ pyprg/aperitivos/tests/tests_indice.py
@@ -13,14 +13,23 @@ def test_status_code(resp):
 
 
 @pytest.mark.parametrize(
-    'titulo',
+    'slug',
     [
         'Video Aperitivo: Motivação',
         'Instalação Wndows'
     ]
 )
-def test_titulo_video(resp, titulo):
-    assert_contains(resp, titulo)
-#
-# def test_conteudo_video(resp):
-#     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/386792293"')
+def test_titulo_video(resp, slug):
+    assert_contains(resp, slug)
+
+
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'instalacao-windows'
+    ]
+)
+def test_link_video(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
