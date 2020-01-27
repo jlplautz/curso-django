905992702c7892832756ae94f493d9de95bc22d1
61246880 <jorge.plautz@gmail.com>
2020-01-27 11:33:42 -0300

    Verificado acesso atributos no templates
    close #141

--- pyprg/aperitivos/views.py
+++ pyprg/aperitivos/views.py
@@ -6,13 +6,21 @@ from django.shortcuts import render
 # No file views.py da app aperitivos  criar uma função chamada video que vai receber
 # o request que vai rendenrizar  a pagina do meu video.
 
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+
 videos = [
-    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 386792293},
-    {'slug': 'instalacao-windows', 'titulo': 'Instalação Wndows', 'vimeo_id': 387145176},
+    Video('motivacao', 'Video Aperitivo: Motivação', 386792293),
+    Video('instalacao-windows', 'Instalação Wndows', 387145176),
 ]
 
 
-videos_dct = {dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
 
 
 def indice(request):
