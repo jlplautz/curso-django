fea60dc181340d1d2563a75f592e85f76d5335e7
61246880 <jorge.plautz@gmail.com>
2020-01-27 00:11:42 -0300

    Ciado a lista de elementos com For close #137

--- pyprg/aperitivos/views.py
+++ pyprg/aperitivos/views.py
@@ -6,10 +6,19 @@ from django.shortcuts import render
 # No file views.py da app aperitivos  criar uma função chamada video que vai receber
 # o request que vai rendenrizar  a pagina do meu video.
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 386792293},
+    {'slug': 'instalacao-windows', 'titulo': 'Instalação Wndows', 'vimeo_id': 387145176},
+]
+
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
+
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 386792293},
-        'instalacao-windows': {'titulo': 'Instalação Wndows', 'vimeo_id': 387145176},
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
