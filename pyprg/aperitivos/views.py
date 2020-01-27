4da22ac5cfa0c1810a62e67885e8511b4f38cd11
61246880 <jorge.plautz@gmail.com>
2020-01-27 20:05:45 -0300

    Verificado acessoa metodo sem parametro no template
    close #143

--- pyprg/aperitivos/views.py
+++ pyprg/aperitivos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 
 # Create your views here.
@@ -12,6 +13,9 @@ class Video:
         self.slug = slug
         self.titulo = titulo
         self.vimeo_id = vimeo_id
+
+    def get_absolute_url(self):
+        return reverse('aperitivos:video', args=(self.slug,))
 
 
 videos = [
